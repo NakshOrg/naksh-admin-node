98858bba48f7151e5255446ef0cf60a7fe0e3676
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2021-08-02 19:46:54 +0530

    logger fix

--- src/app.js
+++ src/app.js
@@ -3,18 +3,20 @@ const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const { contextMiddleware } = require('./helpers/logger');
+
+const { notFound } = require('./middlewares/notFound');
+const { errorHandler } = require('./middlewares/errorHandler');
+
+const wallet = require('./routes/account');
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-const { contextMiddleware } = require('./helpers/logger');
-
-const wallet = require('./routes/account');
-
-const { notFound } = require('./middlewares/notFound');
-const { errorHandler } = require('./middlewares/errorHandler');
+app.use(contextMiddleware);
 
 app.use('/api/account', wallet);
 
