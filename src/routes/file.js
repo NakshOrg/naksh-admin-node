86dc95a3343943a7b5c301d76b0eda8084818870
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2022-02-03 02:15:15 +0530

    file general route

--- src/routes/file.js
+++ src/routes/file.js
@@ -9,6 +9,6 @@ const { s3PutpresignedUrl } = require('../controllers/file');
 const { auth, restrictAdmin } = require('../middlewares/auth');
 
 router.route('/upload')
-.post( celebrate({ [Segments.BODY]: s3PutpresignedUrlBody }), auth, restrictAdmin, s3PutpresignedUrl );
+.post( celebrate({ [Segments.BODY]: s3PutpresignedUrlBody }), s3PutpresignedUrl );
 
 module.exports = router;
