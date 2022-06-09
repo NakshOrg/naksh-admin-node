1512bb0aa795a770b55dc353d4ec4daa14613bd7
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2022-06-09 23:45:53 +0530

    cover gradient default 3

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -34,7 +34,7 @@ exports.updateArtist = asyncHandler(async (req, res, next) => {
         }
     }
 
-    const artist = await Artist.findOneAndUpdate({ _id: req.query.id }, req.body, { new: true, setDefaultsOnInsert: false });
+    const artist = await Artist.findOneAndUpdate({ _id: req.query.id }, req.body, { new: true, setDefaultsOnInsert: true });
 
     if(!artist) {
         return next(new ErrorResponse(404, "artist not found"));
