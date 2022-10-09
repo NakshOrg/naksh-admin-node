9a35a41e4850384af6adee94ad5cf0e940f9374e
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2022-10-09 11:26:53 +0530

    file auth rem

--- src/routes/nft.js
+++ src/routes/nft.js
@@ -21,7 +21,7 @@ const {
 const { auth, restrictAdmin } = require('../middlewares/auth');
 
 router.route('/upload')
-.post( auth, restrictAdmin, uploadImageToIPFS );
+.post( uploadImageToIPFS );
 
 router.route('/trending')
 .patch( celebrate({ [Segments.QUERY]: updateTrendingNftQuery, [Segments.BODY]: updateTrendingNftBody }), updateTrendingNft )
