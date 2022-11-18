141a90393e093f8b1ad9811088c2374256dfb2e4
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2022-11-18 13:22:25 +0530

    collection image

--- src/validators/file.js
+++ src/validators/file.js
@@ -2,7 +2,7 @@ const { Joi } = require('celebrate');
 
 exports.s3PutpresignedUrlBody = Joi.object().keys({
 
-    module: Joi.string().equal("organization", "artist", "artistImage", "collectionImage", "collectionCover").required(),
+    module: Joi.string().equal("organization", "artist", "artistImage", "collection").required(),
     artist: Joi.string().alphanum().length(24),
     totalFiles: Joi.number().min(1).max(10).required()
     
